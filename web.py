53340941b141eb5c724b6dcef84b4367f7e264fa
Kulonful <akulonful@gmail.com>
2018-05-08 11:17:45 +0300

    Adding save users in config-file

--- web.py
+++ web.py
@@ -1,13 +1,33 @@
 from flask import Flask, request, session
+import json
 
 app = Flask(__name__)
+users = {'asd': 2143}
+with open('users.json', 'r') as f:
+    users = json.loads(f.read())
+
+def save():
+    with open('users.json', 'w') as f:
+        f.write(json.dumps(users))
 
 @app.route('/admin')
 def adminPage():
+    save()
     return 'adminpage'
 
-@app.route('/user/', methods = ['GET', 'POST'])
+@app.route('/user', methods = ['GET', 'POST'])
 def userPage():
-    return 'userpage'
+    if request.method == 'GET':
+        print('todo')
+        # TODO
+    elif request.method == 'POST':
+        print('todo')
+        # TODO
+
+@app.route('/reset'):
+    global users
+    users = {}
+    save()
+    return 'success reset', 200
 
 app.run(host = '0.0.0.0', port = 80)
