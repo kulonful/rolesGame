2b8311a12169616d375998bb9842b3b414769b78
Kulonful <akulonful@gmail.com>
2018-05-08 11:00:38 +0300

    Adding base routing

--- web.py
+++ web.py
@@ -1,2 +1,13 @@
-def a():
-    print(True)
+from flask import Flask, request, session
+
+app = Flask(__name__)
+
+@app.route('/admin')
+def adminPage():
+    return 'adminpage'
+
+@app.route('/user/', methods = ['GET', 'POST'])
+def userPage():
+    return 'userpage'
+
+app.run(host = '0.0.0.0', port = 80)
